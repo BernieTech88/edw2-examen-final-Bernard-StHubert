18ef6a25932b22045eb04563e4b8d931e811a3ce
BernieTech88 <brprointech@gmail.com>
2026-07-15 09:24:03 -0700

    Modification  dans index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$nomEtudiant = "À remplacer par votre nom";
+$nomEtudiant = "Bernard St-Hubert ";
 
 $titre = "Examen final EDW2";
 $sousTitre = "Déploiement d’un projet PHP existant sur N0C";
